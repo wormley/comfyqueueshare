7abe296506c5626ebd9d06e99c32d5eb3fb1ea00
Steve Wormley <steve@wormley.com>
2023-11-03 11:59:09 -0700

    FIRST!

---
+++ README.md
@@ -0,0 +1,14 @@
+Really simplistic script to distribute queued prompts.
+
+Takes a primary node and 1 or more secondary nodes, when it sees the primary has >0 pending in queue it tries to move them to idle secondary nodes.
+
+python3 queueshare.py http://primary:8188 http://secondary1:8188 http://secondary2.8188 ...
+
+Notes:
+
+Must have been started with --listen if on different IP addresses.
+
+Custom nodes, checkpoints, loras etc must match.
+
+No intelligence is done with output files, they're just stored locally on each node.
+
---
+++ queueshare.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import sys
+import os
+import time
+
+delay=5
+long_delay =delay*6
+# Primary queue is first argument
+pri = sys.argv[1]
+# Secondary queues follow
+sec = sys.argv[2:]
+
+
+while(1):
+    moved=0
+    try:
+        # check to see if there's any pending items
+        pri_info = requests.get(pri+"/prompt").json()
+        if (pri_info['exec_info']['queue_remaining'] == 0 ):
+            time.sleep(delay)
+            continue
+        print("Found items in queue, checking for available secondaries")
+        # walk the list of secondary queues, look for any that are
+        # empty and have no running jobs
+    except Exception as error: 
+        print("Exception on primary:", error)
+        print("Delaying before reattempt")
+        time.sleep(long_delay)
+        continue
+
+    for i in sec:
+        try:
+            info = requests.get(i+"/prompt").json()
+            if (info['exec_info']['queue_remaining'] != 0 ):
+                continue
+        except:
+            print("Error while checking: "+i)
+            print("Possible node outage")
+            continue
+        queue = requests.get(i+"/queue").json()
+        if (len(queue['queue_running']) > 0 ):
+            continue
+        print("Secondary: "+i+" has queue space, attempting to move prompt")
+        # now, attempt to grab a primary item
+        pri_queue = requests.get(pri+"/queue").json()
+        if (len(pri_queue['queue_pending']) == 0 ):
+            # Take a break, nothing to pass on, let the outer loop... well... loop
+            break
+        our_item = pri_queue['queue_pending'][0]
+        pri_uuid = our_item[1]
+        # attempt to delete
+        d = requests.post(pri+"/queue", json={"delete": [pri_uuid]})
+        print(d.text)
+        # sadly, that doesn't return a useful status code, so we check the queue again
+        pri_queue = requests.get(pri+"/queue").json()
+        if (pri_queue['queue_running'][0][1] == pri_uuid):
+            # too slow
+            continue
+        # finally, send the item to the designated secondary
+        new_req = requests.post(i+"/prompt", json={"prompt":our_item[2] })
+        print("sent prompt: "+(new_req.text))
+        moved=1
+    if (moved == 0 ):
+        # all queues full, take a little nap
+        print("No objects moved, queues likely full")
+        time.sleep(delay)
